b6074bf8069e543cd0446af23949c8b7784fc913
Priyanka <sahoopriyanka.rbp888@gmail.com>
2021-07-18 18:38:38 +0530

    intial commit by TL

---
+++ Priyanka/src/com/priyanka/beans/Utility.java
@@ -0,0 +1,15 @@
+package com.priyanka.beans;
+
+import java.util.Date;
+
+ 
+
+public class Utility {
+    
+    public Date getSystemDate() {
+        return new Date();                       // ctrl+shift+o
+    }
+
+ 
+
+}
