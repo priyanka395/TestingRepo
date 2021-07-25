2a3765f667e09b96446e18a65318655fa18390b9
Priyanka <sahoopriyanka.rbp888@gmail.com>
2021-07-25 16:59:27 +0530

    Created Test.java by the DEV2

---
+++ Priyanka/src/com/priyanka/test/Test.java
@@ -0,0 +1,14 @@
+package com.priyanka.test;
+
+import com.priyanka.beans.Utility;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Utility u = new Utility();
+		System.out.println(u.getSystemDate());
+
+	}
+
+}
